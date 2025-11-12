7a6e816126b7391ed49cfec563681242c1051617
NUR FADILLAH <2511500031@mahasiswa.atmaluhur.ac.id>
2025-11-12 13:00:30 +0700

    menambahkan tombol kirim dan batal

--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -23,7 +23,7 @@ endif;
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>pendaftaran profil pengunjung</title>
+  <title>judul halaman</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
