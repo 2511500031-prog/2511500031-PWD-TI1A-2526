d8dcb2d7ad511c4fddae2ec05c69e260de643a78
NUR FADILLAH <2511500031@mahasiswa.atmaluhur.ac.id>
2025-11-12 12:47:33 +0700

    membuat session

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -1,10 +1,17 @@
 <?php
 session_start();
-$sesnama = $_POST["txtNama"];
-$sesemail = $_POST["txtEmail"];
-$sespesan = $_POST["txtPesan"];
+if (isset($_POST)['kirim'])){
+$_SESSION["sesnim"] = $sesnim;
 $_SESSION["sesnama"] = $sesnama;
-$_SESSION["sesemail"] = $sesemail;
-$_SESSION["sespesan"] = $sespesan;
+$_SESSION["sestempat"] = $sestempat;
+$_SESSION["sestanggallahir"] = $sestanggallahir;
+$_SESSION["seshobi"] = $seshobi;
+$_SESSION["sespasangan"] = $sespasangan;
+$_SESSION["sespekerjaan"] = $sespekerjaan;
+$_SESSION["sesortu"] = $sesortu;
+$_SESSION["seskakak"] = $seskakak;
+$_SESSION["sesadik"] = $sesadik;
+
 header("location: index.php");
+}
 ?>
