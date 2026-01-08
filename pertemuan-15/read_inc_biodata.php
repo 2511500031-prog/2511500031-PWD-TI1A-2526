5d64eefeaaa7b8030b6584b290e40381e1ee58cc
NUR FADILLAH <2511500031@mahasiswa.atmaluhur.ac.id>
2026-01-08 14:32:11 +0700

    mengupdate fieldBiodata di index.php

--- pertemuan-15/read_inc_biodata.php
+++ pertemuan-15/read_inc_biodata.php
@@ -1,13 +1,21 @@
 <?php
 require 'koneksi.php';
 
-$fieldContact = [
-  "nama" => ["label" => "Nama:", "suffix" => ""],
-  "email" => ["label" => "Email:", "suffix" => ""],
-  "pesan" => ["label" => "Pesan Anda:", "suffix" => ""]
+$fieldBiodata = [
+  "nim" => ["label" => "nim:", "suffix" => ""],
+  "nama" => ["label" => "nama:", "suffix" => ""],
+  "tempat_tinggal" => ["label" => "tempat_tinggal:", "suffix" => ""],
+  "tanggal_lahir" => ["label" => "tanggal_lahir:", "suffix" => ""],
+  "hobi" => ["label" => "hobi:", "suffix" => ""],
+  "pekerjaan" => ["label" => "pekerjaan:", "suffix" => ""],
+  "pasangan" => ["label" => "pasangan:", "suffix" => ""],
+  "orang_tua" => ["label" => "orang_tua:", "suffix" => ""],
+  "kakak" => ["label" => "kakak:", "suffix" => ""],
+  "adik" => ["label" => "adik:", "suffix" => ""]
+
 ];
 
-$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$sql = "SELECT * FROM tabel_biodata ORDER BY bid DESC";
 $q = mysqli_query($conn, $sql);
 if (!$q) {
   echo "<p>Gagal membaca data tamu: " . htmlspecialchars(mysqli_error($conn)) . "</p>";
@@ -15,10 +23,19 @@ if (!$q) {
   echo "<p>Belum ada data tamu yang tersimpan.</p>";
 } else {
   while ($row = mysqli_fetch_assoc($q)) {
-    $arrContact = [
-      "nama"  => $row["cnama"]  ?? "",
-      "email" => $row["cemail"] ?? "",
-      "pesan" => $row["cpesan"] ?? "",
+    $arrBiodata= [
+      "nim"  => $row["bnim"]  ?? "",
+      "nama" => $row["bnama"] ?? "",
+      "tempat_tinggal" => $row["btempat_tinggal"] ?? "",
+      "tanggal_lahir" => $row["btanggal_lahir"] ?? "",
+      "hobi" => $row["bhobi"] ?? "",
+      "pekerjaan" => $row["bpekerjaan"] ?? "",
+      "pasangan" => $row["bpasangan"] ?? "",
+      "orang_tua" => $row["borang_tua"] ?? "",
+      "kakak" => $row["bkakak"] ?? "",
+      "adik" => $row["badik"] ?? "",
+
+
     ];
     echo tampilkanBiodata($fieldContact, $arrContact);
   }
